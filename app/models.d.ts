5379236f35c1d343652918f23f015f86b5d16314
Vadym Perkin <vperkinv@gmail.com>
2025-01-28 10:51:24 +0100

    Created common type for config file for fixtures

--- app/models.d.ts
+++ app/models.d.ts
@@ -1,30 +1,32 @@
+type DefaultAdmin = {
+  defaultAdmin: {
+    username: string;
+    password: string;
+  };
+};
+
+type DefaultIngredients = {
+  testOptions: {
+    ingredients: {
+      size: "large" | "medium" | "small";
+      flavour: "Cheese" | "Pepperoni" | "Supreme" | "Veggie Delight";
+      sauce: "Marinara" | "Buffalo" | "Barbeque";
+      topping: "Onions" | "Green Olive" | "Tomatoes";
+      quantity: number;
+    }[];
+  };
+};
+
+type Ingredients = {
+  size: "large" | "medium" | "small";
+  flavour: "Cheese" | "Pepperoni" | "Supreme" | "Veggie Delight";
+  sauce: "Marinara" | "Buffalo" | "Barbeque";
+  topping: "Onions" | "Green Olive" | "Tomatoes";
+  quantity: number;
+};
+
 declare global {
-  type DefaultAdmin = {
-    defaultAdmin: {
-      username: string;
-      password: string;
-    };
-  };
-
-  type DefaultIngredients = {
-    testOptions: {
-      ingredients: {
-        size: "large" | "medium" | "small";
-        flavour: "Cheese" | "Pepperoni" | "Supreme" | "Veggie Delight";
-        sauce: "Marinara" | "Buffalo" | "Barbeque";
-        topping: "Onions" | "Green Olive" | "Tomatoes";
-        quantity: number;
-      }[];
-    };
-  };
-
-  type Ingredients = {
-    size: "large" | "medium" | "small";
-    flavour: "Cheese" | "Pepperoni" | "Supreme" | "Veggie Delight";
-    sauce: "Marinara" | "Buffalo" | "Barbeque";
-    topping: "Onions" | "Green Olive" | "Tomatoes";
-    quantity: number;
-  };
-}
+  interface ConfigTypes extends DefaultAdmin, DefaultIngredients, Ingredients {}
+} // Use in playwright.config.ts as a common type
 
 export {};
